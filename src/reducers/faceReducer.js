0d31682a46f2cbc2fbc57e9e3c26a8bc9f0c1004
drmeloy <drmeloy@gmail.com>
2020-02-12 14:32:30 -0700

    eat snacks reducer done

--- src/reducers/faceReducer.js
+++ src/reducers/faceReducer.js
@@ -1,10 +1,12 @@
-import { DRINK_COFFEE } from '../actions/faceActions';
+import { DRINK_COFFEE, EAT_SNACK } from '../actions/faceActions';
 
 
 export default function reducer(state, action){
   switch(action.type){
     case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
+    case EAT_SNACK:
+      return { ...state, snacks: state.snacks + 1 };
     default:
       return state;
   }
--- src/reducers/faceReducer.test.js
+++ src/reducers/faceReducer.test.js
@@ -1,5 +1,5 @@
 import reducer from './faceReducer';
-import { drinkCoffee } from '../actions/faceActions';
+import { drinkCoffee, eatSnack } from '../actions/faceActions';
 
 describe('face reducer', () => {
   it('does nothing when given an unexpected action', () => {
@@ -13,5 +13,12 @@ describe('face reducer', () => {
     const initialState = { coffees: 0 };
     const newState = reducer(initialState, action);
     expect(newState).toEqual({ coffees: 1 });
+  });
+
+  it('handles the EAT_SNACK action', () => {
+    const action = eatSnack();
+    const initialState = { snacks: 1 };
+    const newState = reducer(initialState, action);
+    expect(newState).toEqual({ snacks: 2 });
   });
 });
